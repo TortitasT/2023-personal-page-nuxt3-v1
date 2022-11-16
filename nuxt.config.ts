79ed3307070d16dd6a37ae20cb34b03c403fd1ca
TortitasT <victorgf2011@gmail.com>
2022-11-16 21:53:05 +0100

    lighthouse 100%

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,15 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=500, initial-scale=1",
+      title: "Tortitas",
+      meta: [
+        { name: "description", content: "Tortitas personal page." },
+      ],
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+});
