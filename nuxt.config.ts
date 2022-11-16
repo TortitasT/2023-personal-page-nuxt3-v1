d5177670a3bc73f70c21ddeeb661a17b3c2685e5
TortitasT <victorgf2011@gmail.com>
2022-11-16 21:55:38 +0100

    fix viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: "utf-16",
-      viewport: "width=500, initial-scale=1",
+      viewport: "width=device-width, initial-scale=1",
       title: "Tortitas",
       meta: [
         { name: "description", content: "Tortitas personal page." },
